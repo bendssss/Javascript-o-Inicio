ce474fcd46d1563798c59dfcced80ae024b4c0d2
bendsss <bendroder@gmail.com>
2025-11-10 20:15:06 -0300

    atividade 01

---
+++ aula 003/atividade001/app.js
@@ -0,0 +1,12 @@
+let n1 = 0, n2 = 0,n3 = 0,n4 = 0,media = 0
+n1 = 7
+n2 = 5.4
+n3 = 3.27
+n4 = 8
+media = (n1 + n2 + n3 + n4)/4
+
+console.log("Média = "+ media.toFixed(2))   
+
+
+//1- como fazer para o  resultado apresentar apenas duas casas decimais
+//2-como fazer para o progama solicitar notas para os usuarios 
